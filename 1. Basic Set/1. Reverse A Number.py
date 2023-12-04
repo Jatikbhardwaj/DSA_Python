7b046153a4a286e5a7778770554d9d301de01f20
Jatik Bhardwaj <jatik.bhardwaj@tmconnected.com>
2023-12-04 07:03:35 +0530

    Reverse a number

---
+++ 1. Basic Set/1. Reverse A Number.py
@@ -0,0 +1,17 @@
+t = int(input())
+for i in range(t):
+  num = int(input())
+  reverse = 0
+  while(num):
+    rem = num % 10
+    reverse = reverse * 10 + rem
+    num = num // 10
+  print(reverse)
+
+# Alternative method
+
+t = int(input())
+for i in range(t):
+    num = int(input())
+    reverse = int(str(num)[::-1])
+    print(reverse)
