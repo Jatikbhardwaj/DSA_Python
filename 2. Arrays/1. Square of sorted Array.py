812bccd3a985bec5098bfcf2338ce705306667cc
Jatik Bhardwaj <jatik.bhardwaj@tmconnected.com>
2023-12-04 09:00:05 +0530

    Arrays

---
+++ 2. Arrays/1. Square of sorted Array.py
@@ -0,0 +1,22 @@
+def square_sorted(arr: list[int]) -> list[int]:
+    result = []
+    left, right = 0, len(arr)-1
+
+    while left <= right:
+        if abs(arr[left]) >= abs(arr[right]):
+            result.append(arr[left] * arr[left])
+            left += 1
+        else:
+            result.append(arr[right] * arr[right])
+            right -= 1
+    return result[::-1]
+
+n = int(input())
+arr = list(map(int, input().split()))
+resultant_array = square_sorted(arr)
+print(*resultant_array)
+
+
+
+
+
