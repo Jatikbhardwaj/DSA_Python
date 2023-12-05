9b2fcd38678414b9554764baa84d03e41d8fcda7
Jatik Bhardwaj <jatik.bhardwaj@tmconnected.com>
2023-12-05 09:46:08 +0530

    hashtable(dictionary) implementation

--- 2. Arrays/2. Treasure Room easy.py
+++ 2. Arrays/2. Treasure Room easy.py
@@ -1,8 +1,12 @@
 # Input
 # Two integers on the first line, N W.
 # N integers on the second line where the i-th integer is li.
+# i/p:- 
+# 7 8
+# 7 5 3 6 9 2 9
 # Output
 # Any two distinct integers i and j such that li+lj=W. If it is impossible, output −1
+# o/p:-2 3
 
 n1, n2 = map(int,input().split())
 arr = list(map(int,input().split()))
