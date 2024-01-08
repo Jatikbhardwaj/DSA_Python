d25fa3ea58092e40a52ee49a2d2faa8385da20df
Jatik Bhardwaj <jatik.bhardwaj@tmconnected.com>
2024-01-08 21:35:07 +0530

    solved new problem

--- 4. Advanced Arrays and Matrix.py/Maximum sum of k consecutive elements.py
+++ 4. Advanced Arrays and Matrix.py/Maximum sum of k consecutive elements.py
@@ -14,13 +14,23 @@
 # TC = O(n*k)
 # SC = O(1)
 
-# USING SLIDING WINDOW TECHNIQUE 
+# USING SLIDING WINDOW TECHNIQUE
 
 n = int(input())
-array = list(map(int,input().split()))
-k = int (input())
-max_sum = float('inf')
+array = list(map(int, input().split()))
+k = int(input())
+max_sum = float("-inf")
+sum = 0
+for i in range(k):
+    sum += array[i]
 
+curr_sum = sum
 
+for i in range(k, n):
+    curr_sum += array[i] - array[i - k]
+    max_sum = max(max_sum, curr_sum)
 
+print(max_sum)
 
+# TC = O(n)
+# Sc = O(1)
