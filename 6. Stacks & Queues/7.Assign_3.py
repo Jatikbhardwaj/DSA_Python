f9106320b516583ef018f90382fa329c2e575556
Jatik Bhardwaj <jatik.bhardwaj@tmconnected.com>
2024-05-19 12:29:09 +0530

    postfix question

---
+++ 6. Stacks & Queues/7.Assign_3.py
@@ -0,0 +1,20 @@
+def evaluate_postfix(expresssion):
+    stack = []
+    for char in expresssion:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            if char == '+':
+                stack.append(a + b)
+            elif char == '-':
+                stack.append(a - b)
+            elif char == '*':
+                stack.append(a * b)
+            elif char == '/':
+                stack.append(int(a/b))
+    return stack[-1] if stack else None
+
+expression = list(map(str,input().split()))
+result = print(evaluate_postfix(expression))
