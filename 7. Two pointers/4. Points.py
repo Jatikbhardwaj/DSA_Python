f575bf374918f15fbbf4d345ab70cb7c7e137df4
Jatik Bhardwaj <jatik.bhardwaj@tmconnected.com>
2024-05-26 11:12:04 +0530

    combination formula +  two pointer

--- 7. Two pointers/4. Points.py
+++ 7. Two pointers/4. Points.py
@@ -1,21 +1,26 @@
 def count_triplets(points, d):
     n = len(points)
     count = 0
-    for i in range(n-2):
-        j = i
-        while j< n and points[j] - points[i] <= d:
-            j+=1
-        num_points = j-i
+    
+    j = 0
+    
+    for i in range(n):
 
-        if num_points>=3:
-            count+=(num_points -1 )* (num_points - 2) // 2
+        while j < n and points[j] - points[i] <= d:
+            j += 1
+        
+
+        num_points = j - i
+
+        if num_points >= 3:
+            count += (num_points - 1) * (num_points - 2) // 2
+    
     return count
+
 
 n, d = map(int, input().split())
 points = list(map(int, input().split()))
-
 points.sort()
-
 result = count_triplets(points, d)
 
 print(result)
