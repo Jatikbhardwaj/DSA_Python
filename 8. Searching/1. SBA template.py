4812df320d65b0b28ffe44fd0ebdb587145fe0f1
Jatik Bhardwaj <jatik.bhardwaj@tmconnected.com>
2024-09-09 08:35:52 +0530

    SBA Template

---
+++ 8. Searching/1. SBA template.py
@@ -0,0 +1,20 @@
+def binary_search_first_one(n, arr):
+    L = 0
+    R = n - 1
+    while L + 1 < R:
+        M = (L + R) // 2
+        if arr[M] == 0:
+            L = M
+        else:
+            R = M
+
+    if R < n:
+        return R
+    else:
+        return -1
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+result = binary_search_first_one(n, arr)
+print(result)
